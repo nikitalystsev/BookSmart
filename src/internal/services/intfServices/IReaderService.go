b7adb9ab1c9fe1fddabd64ac5d5bf7263eb49ebd
nikitalystsev <96741101+nikitalystsev@users.noreply.github.com>
2024-07-26 15:09:45 +0300

    поправил uml, вынес пару методов reader-а в интерфейс

--- src/internal/services/intfServices/IReaderService.go
+++ src/internal/services/intfServices/IReaderService.go
@@ -4,11 +4,14 @@ import (
 	"BookSmart/internal/dto"
 	"BookSmart/internal/models"
 	"context"
+	"github.com/google/uuid"
 )
 
 type IReaderService interface {
 	SignUp(ctx context.Context, reader *models.ReaderModel) error
 	SignIn(ctx context.Context, reader *dto.ReaderLoginDTO) error
+	RefreshTokens(ctx context.Context, refreshToken string) (Tokens, error)
+	AddToFavorites(ctx context.Context, readerID, bookID uuid.UUID) error
 }
 
 type Tokens struct {
